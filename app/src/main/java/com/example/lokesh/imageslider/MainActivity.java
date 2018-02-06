9407ec826252c8820e753abdcd93cd1e0f6f1cca
posakya <roshanposakya07@gmail.com>
2018-02-06 19:18:26 +0545

    initial commit

---
+++ app/src/main/java/com/example/lokesh/imageslider/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.lokesh.imageslider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewFlipper mViewFlipper;
+    private GestureDetector mGestureDetector;
+
+    int[] resources = {
+            R.drawable.samosa,
+            R.drawable.ente,
+            R.drawable.fish_curry,
+            R.drawable.beilagen,
+            R.drawable.soup,
+            R.drawable.tandoori
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get the ViewFlipper
+        mViewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+
+        // Add all the images to the ViewFlipper
+        for (int i = 0; i < resources.length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(resources[i]);
+            mViewFlipper.addView(imageView);
+        }
+
+        // Set in/out flipping animations
+        mViewFlipper.setInAnimation(this, android.R.anim.fade_in);
+        mViewFlipper.setOutAnimation(this, android.R.anim.fade_out);
+
+        CustomGestureDetector customGestureDetector = new CustomGestureDetector();
+        mGestureDetector = new GestureDetector(this, customGestureDetector);
+
+    }
+
+    private class CustomGestureDetector extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+
+            // Swipe left (next)
+            if (e1.getX() > e2.getX()) {
+                mViewFlipper.showNext();
+            }
+
+            // Swipe right (previous)
+            if (e1.getX() < e2.getX()) {
+                mViewFlipper.showPrevious();
+            }
+
+            return super.onFling(e1, e2, velocityX, velocityY);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mGestureDetector.onTouchEvent(event);
+
+        return super.onTouchEvent(event);
+    }
+
+
+}
